96bddab457439b57dfa1197ea1d7e043c47c195c
Jermaine Carelse <jcarelse@live.co.za>
2022-04-29 09:22:29 +0200

    Updated employee file
    Checking salary for loan eligibility

--- employee.py
+++ employee.py
@@ -1,7 +1,11 @@
 def show_employee():
     name = input("Enter employee name: ")
     salary = int(input("Enter employee salary: "))
-    print("Your name is: ", name + " " + "and your salary is: ", salary)
+    if salary >= 20000:
+        print("Your name is: ", name + " " + "and your salary is: ", salary)
+    else:
+        print("You do not qualify for company loan")
+
 
 show_employee()
 
